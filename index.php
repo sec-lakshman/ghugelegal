4e2c42eadea871f1258d39e4e87ab0319c813761
Lakshman Pawar <107903092+sec-lakshman@users.noreply.github.com>
2024-09-13 19:20:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -88,18 +88,18 @@
                     </b>
                 </p>
     
-                <!-- Appointment and Call Buttons -->
+            <!-- Appointment and Call Buttons -->
                 <div style="margin-top: 20px;">
                     <!-- Online Appointment Button -->
                     <a href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/ghugelegal-info/15min'}); return false;">
-                        <button class="button-63" style="width: 100%; padding: 10px; margin-bottom: 10px;" role="button">
+                        <button class="button-63" style="height: 40px; padding: 5px 20px; margin-bottom: 10px;" role="button">
                             Click here to book an online appointment
                         </button>
                     </a>
     
                     <!-- Call Button -->
                     <a href="tel:+14168169249">
-                        <button class="button-63" style="width: 100%; padding: 10px;" role="button">
+                        <button class="button-63" style="height: 40px; padding: 5px 20px;" role="button">
                             Call +1 416-816-9249
                         </button>
                     </a>
